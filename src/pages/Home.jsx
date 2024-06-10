468e81ba8dbb5d74bd9a71228ffa51d8af5a3e70
Nicolly Moreira <nicollymoreira840@gmail.com>
2024-06-10 03:45:13 +0000

    style: posicionamento do card no centro da tela

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,25 @@
+import { CardContiner } from "./Home.styles";
+import  IconStarImg  from "../assets/icon-star.svg"
 
 export function Home() {
   return (
-    <div>Home</div>
+   <CardContiner>
+    <IconContainer>
+      <img src="{iconStarImg}" alt="ícone de estrela" />
+    </IconContainer>
+
+    <h1>Como doi o atendimento?</h1>
+    <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
+
+    <ButtonContainer>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+    </ButtonContainer>
+
+    <button>Enviar</button>
+   </CardContiner>
   )
 }
---
+++ src/pages/Home.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const CardContiner = styled.div`
+    background: ${props => props.theme.CardContiner};
+    max-width: 41.2rem;
+    padding: 3.2rem;
+`
+
+export const IconContainer = style.div``
+
+export const ButtonContainer = styled.div``
