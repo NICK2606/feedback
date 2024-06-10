3782cbbac824b27239e8c449a26f82a3db45656e
Nicolly Moreira <nicollymoreira840@gmail.com>
2024-06-10 03:53:50 +0000

    style: estilização do h1 e do p do card

--- src/pages/Home.styles.js
+++ src/pages/Home.styles.js
@@ -4,6 +4,21 @@ export const CardContiner = styled.div`
     background: ${props => props.theme.CardContiner};
     max-width: 41.2rem;
     padding: 3.2rem;
+    border-radius: 3rem;
+
+    h1{
+      font-size: 2.8rem;
+      color: $(props => props.theme.white);
+      margin-botton: 1.5rem;
+       text-align: center;
+    }
+
+    p{
+       font-size: 1.5rem;
+       line-height: 2.4rem;
+       color: $(props => props.theme.textColor);
+       text-align: center; 
+    }
 `
 
 export const IconContainer = style.div``
